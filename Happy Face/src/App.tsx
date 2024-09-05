95cefdf5e70d78ae1300119b9fcdf1d71126f294
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-09-05 18:16:48 +0200

    added and setup router and routes

--- Happy Face/src/App.tsx
+++ Happy Face/src/App.tsx
@@ -1,10 +1,16 @@
+import React from 'react';
+import { AuthProvider } from './contexts/AuthContext';
+import { TokenAuthProvider } from './contexts/TokenAuthContext';
+import AppRouter from './routes/AppRouter';
 
-function App() {
-
+const App: React.FC = () => {
   return (
-    <div>
-    </div>
-  )
-}
+    <AuthProvider>
+      <TokenAuthProvider>
+        <AppRouter />
+      </TokenAuthProvider>
+    </AuthProvider>
+  );
+};
 
-export default App
+export default App;
