99ad71be15f714c94d361932e4a8ae2a0cb2647c
shawn-ii <106849147+shawn-ii@users.noreply.github.com>
2024-09-06 15:52:23 +0200

    navbar shadcn ui components and sample menus array

--- Happy Face/src/components/navbar.tsx
+++ Happy Face/src/components/navbar.tsx
@@ -18,6 +18,61 @@ import {User} from "@/models";
 import {Menu} from "@/models/Menu.ts";
 
 
+
+
+//const handleNewAppointment = () => {
+//    console.log("New appointment clicked");
+//};
+//
+//const handleViewAppointments = () => {
+//    console.log("View all appointments clicked");
+//};
+//
+//const handleDeleteAppointment = () => {
+//    console.log("Delete appointment clicked");
+//};
+//
+//const menus: Menu[] = [
+//    {
+//        name: "Appointment",
+//        items: [
+//            { label: "New Appointment", shortcut: "⌘N", onClick: handleNewAppointment },
+//            { label: "Edit Appointment", shortcut: "⌘E" },
+//            { label: "View All Appointments", shortcut: "⌘L", onClick: handleViewAppointments },
+//            'separator',
+//            { label: "Reschedule Appointment", shortcut: "⌘R" },
+//            { label: "Delete Appointment", shortcut: "⌘D", onClick: handleDeleteAppointment },
+//        ],
+//    },
+//    {
+//        name: "Patient",
+//        items: [
+//            { label: "Add New Patient", shortcut: "⌘P" },
+//            { label: "View Patient List", shortcut: "⌘V" },
+//            'separator',
+//            { label: "View Patient Profile", shortcut: "⌘O" },
+//        ],
+//    },
+//    {
+//        name: "Doctor Schedules",
+//        items: [
+//            { label: "View Doctor Availability", shortcut: "⌘A" },
+//            { label: "Block Doctor Time", shortcut: "⌘T" },
+//        ],
+//    },
+//    {
+//        name: "Billing & Payments",
+//        items: [
+//            { label: "Generate Invoice", shortcut: "⌘I" },
+//            { label: "View Payment Status", shortcut: "⌘Y" },
+//            { label: "View Payment History", shortcut: "⌘H" },
+//        ],
+//    },
+//];
+
+
+
+
 // Props for the Navbar
 interface NavbarProps {
     menus: Menu[];
