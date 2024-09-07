e75894127f43ae0b5e53b54930ff2009a23015fe
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-09-07 15:28:07 +0200

    registered interface for auth service and added abstract methods for user service interface

---
+++ Smile Base/src/main/java/de/vd40xu/smilebase/service/interfaces/IAuthService.java
@@ -0,0 +1,9 @@
+package de.vd40xu.smilebase.service.interfaces;
+
+import de.vd40xu.smilebase.dto.UserDTO;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface IAuthService {
+    void registerUser(UserDTO userDto);
+    UserDetails loginUser(UserDTO userDto);
+}
