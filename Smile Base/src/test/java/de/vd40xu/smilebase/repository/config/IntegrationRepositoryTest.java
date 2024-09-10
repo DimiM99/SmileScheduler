9cb52f57464fc2b13bf79d1ddd3c95d8d7340458
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-09-10 22:01:03 +0200

    added automatic test database configuration for repository integration tests base

--- Smile Base/src/test/java/de/vd40xu/smilebase/repository/config/IntegrationRepositoryTest.java
+++ Smile Base/src/test/java/de/vd40xu/smilebase/repository/config/IntegrationRepositoryTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
 @Transactional(propagation = Propagation.NOT_SUPPORTED)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class IntegrationRepositoryTest {
 
     @Autowired EntityManager entityManager;
