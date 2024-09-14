5d2f3c06c7b0e0d85658eef147fa1e5848a8b87c
shawn-ii <106849147+shawn-ii@users.noreply.github.com>
2024-09-14 14:54:14 +0200

    authService into AuthContext

--- Happy Face/src/contexts/AuthContext.tsx
+++ Happy Face/src/contexts/AuthContext.tsx
@@ -1,22 +1,35 @@
 import React, {createContext, useState, ReactNode} from 'react';
-import {User} from '@/models';
-import {AuthContextType} from "@/models/auth/AuthContextType.ts";
+import {AuthContextType} from "@/models/components/auth/AuthContextType.ts";
+import {AuthService} from "@/services/authService.ts";
+import {LoginRequest} from "@/models/services/requests/LoginRequest.ts";
+import {GetUserResponse} from "@/models/services/responses/GetUserResponse.ts";
 
 export const AuthContext = createContext<AuthContextType | undefined> (undefined);
 
-export const AuthProvider: React.FC<{ children: ReactNode }> = ({children}) => {
-    const [user, setUser] = useState<User | null> (null);
+export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+    const [user, setUser] = useState<GetUserResponse | null>(null);
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const login = async (username: string, password: string): Promise<void> => {
+    const authService = new AuthService();  // Create instance of AuthService
+
+    const login = async (loginRequest: LoginRequest): Promise<void> => {
+        try {
+            await authService.login(loginRequest);
+            const loggedInUser = await authService.getUser();
+            setUser(loggedInUser);
+
+        } catch (error) {
+            console.error('Login failed', error);
+        }
     };
 
+
     const logout = (): void => {
-        setUser (null);
+        authService.clearToken();
+        setUser(null);
     };
 
     return (
-        <AuthContext.Provider value={{user, login, logout}}>
+        <AuthContext.Provider value={{ user, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
