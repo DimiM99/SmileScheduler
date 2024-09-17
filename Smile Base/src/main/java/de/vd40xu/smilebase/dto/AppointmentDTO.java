b2ed2c106735dc8118735660d16de53315cecd38
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-09-17 15:29:35 +0200

    created dto for appointments

---
+++ Smile Base/src/main/java/de/vd40xu/smilebase/dto/AppointmentDTO.java
@@ -0,0 +1,18 @@
+package de.vd40xu.smilebase.dto;
+
+import de.vd40xu.smilebase.model.emuns.AppointmentType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class AppointmentDTO {
+    private Long id;
+    private String title;
+    private Long patientId;
+    private Long doctorId;
+    private LocalDateTime start;
+    private AppointmentType appointmentType;
+}
