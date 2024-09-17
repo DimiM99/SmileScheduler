955e6eab2e275c255b11a85fda923831b7ff6ae6
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-09-18 00:43:59 +0200

    created controller for scheduling api

---
+++ Smile Base/src/main/java/de/vd40xu/smilebase/controller/AppointmentController.java
@@ -0,0 +1,8 @@
+package de.vd40xu.smilebase.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class AppointmentController { }
