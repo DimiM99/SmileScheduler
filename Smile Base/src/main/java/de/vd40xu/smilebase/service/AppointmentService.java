e03428df5a3322724a48d49d020ef2dedf323493
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-09-18 00:43:24 +0200

    added preliminary implementation of AppointmentService

--- Smile Base/src/main/java/de/vd40xu/smilebase/service/AppointmentService.java
+++ Smile Base/src/main/java/de/vd40xu/smilebase/service/AppointmentService.java
@@ -1,14 +1,22 @@
 package de.vd40xu.smilebase.service;
 
 import de.vd40xu.smilebase.dto.AppointmentDTO;
+import de.vd40xu.smilebase.dto.NewAppointmentDTO;
 import de.vd40xu.smilebase.model.Appointment;
+import de.vd40xu.smilebase.model.Patient;
 import de.vd40xu.smilebase.model.emuns.AppointmentType;
+import de.vd40xu.smilebase.model.emuns.UserRole;
 import de.vd40xu.smilebase.repository.AppointmentRepository;
+import de.vd40xu.smilebase.repository.PatientRepository;
+import de.vd40xu.smilebase.repository.UserRepository;
 import de.vd40xu.smilebase.service.interfaces.IAppointmentService;
 import lombok.Setter;
 
 import java.time.*;
+import java.time.temporal.TemporalAdjusters;
 import java.util.List;
+
+import static de.vd40xu.smilebase.service.utility.AppointmentServiceUtils.getFreeSlots;
 
 public class AppointmentService implements IAppointmentService {
 
@@ -16,11 +24,17 @@ public class AppointmentService implements IAppointmentService {
     private final LocalTime clinicCloseTime = LocalTime.of(17, 0);
 
     private final AppointmentRepository appointmentRepository;
+    private final UserRepository userRepository;
+    private final PatientRepository patientRepository;
 
     public AppointmentService(
-            AppointmentRepository appointmentRepository
+            AppointmentRepository appointmentRepository,
+            UserRepository userRepository,
+            PatientRepository patientRepository
     ) {
         this.appointmentRepository = appointmentRepository;
+        this.userRepository = userRepository;
+        this.patientRepository = patientRepository;
     }
 
     @Override
@@ -29,25 +43,94 @@ public class AppointmentService implements IAppointmentService {
             LocalDate date,
             AppointmentType appointmentType,
             boolean weekView
-    ) {}
+    ) {
+        var refDates = new Object() {
+            LocalDateTime startDate = date.atTime(clinicOpenTime);
+            LocalDateTime endDate = date.atTime(clinicCloseTime);
+        };
+        if (weekView) {
+            refDates.startDate = refDates.startDate
+                                            .isBefore(LocalDateTime.now(clock)) ?
+                                                LocalDateTime.now(clock) :
+                                                refDates.startDate.with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+            refDates.endDate = refDates.startDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.FRIDAY));
+        }
+        var appointments = appointmentRepository.findByDoctorIdAndStartBetween(doctorId, refDates.startDate, refDates.endDate);
+        return getFreeSlots(appointments, appointmentType.getDuration(), refDates.startDate, refDates.endDate);
+    }
 
     @Override
-    public Appointment scheduleAppointment(AppointmentDTO appointmentDTO) {
-        return null;
+    public Appointment scheduleAppointment(NewAppointmentDTO appointmentDTO) {
+        Appointment appointment = new Appointment(
+                appointmentDTO.getTitle(),
+                appointmentDTO.getStart(),
+                appointmentDTO.getAppointmentType()
+        );
+        userRepository.findById(appointmentDTO.getDoctorId()).ifPresentOrElse(
+                doc -> {
+                    if (doc.getRole().equals(UserRole.DOCTOR)) {
+                        appointment.setDoctor(doc);
+                    } else {
+                        throw new IllegalArgumentException("User is not a doctor");
+                    }
+                },
+                () -> {
+                    throw new IllegalArgumentException("Doctor not found");
+                }
+        );
+        appointment.setPatient(
+            patientRepository.save(
+                new Patient(
+                        appointmentDTO.getPatient().getName(),
+                        appointmentDTO.getPatient().getBirthdate(),
+                        appointmentDTO.getPatient().getInsuranceNumber(),
+                        appointmentDTO.getPatient().getInsuranceProvider(),
+                        appointmentDTO.getPatient().getEmail()
+                )
+            )
+        );
+        return appointmentRepository.save(appointment);
     }
 
     @Override
     public Appointment getAppointmentById(Long id) {
-        return null;
+        return appointmentRepository.findById(id).orElseThrow();
     }
 
     @Override
-    public Appointment updateAppointment(Appointment appointment) {
-        return null;
+    public Appointment updateAppointment(AppointmentDTO appointmentDTO) {
+        Appointment appointment = appointmentRepository.findById(appointmentDTO.getId()).orElseThrow();
+        if (appointmentDTO.getTitle() != null) {
+            appointment.setTitle(appointmentDTO.getTitle());
+        }
+        if (appointmentDTO.getStart() != null) {
+            appointment.setStart(appointmentDTO.getStart());
+        }
+        if (appointmentDTO.getAppointmentType() != null) {
+            appointment.setAppointmentType(appointmentDTO.getAppointmentType());
+            appointment.setEnd(appointment.getStart().plusMinutes(appointment.getAppointmentType().getDuration()));
+        }
+        if (appointmentDTO.getDoctorId() != null) {
+            userRepository.findById(appointmentDTO.getDoctorId()).ifPresentOrElse(
+                    doc -> {
+                        if (doc.getRole().equals(UserRole.DOCTOR)) {
+                            appointment.setDoctor(doc);
+                        } else {
+                            throw new IllegalArgumentException("User is not a doctor");
+                        }
+                    },
+                    () -> {
+                        throw new IllegalArgumentException("Doctor not found");
+                    }
+            );
+        }
+        return appointmentRepository.save(appointment);
     }
 
     @Override
-    public void deleteAppointment(Long id) {}
+    public void deleteAppointment(Long id) {
+        appointmentRepository.deleteById(id);
+    }
 
     @Setter
     private Clock clock = Clock.systemDefaultZone();
