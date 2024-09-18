924ff7029ce996dbd6004f934bd6a1149391aaee
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-09-18 21:19:27 +0200

    changed some methods' signatures for patient service interface and implemented it

--- Smile Base/src/main/java/de/vd40xu/smilebase/service/interfaces/IPatientService.java
+++ Smile Base/src/main/java/de/vd40xu/smilebase/service/interfaces/IPatientService.java
@@ -1,14 +1,15 @@
 package de.vd40xu.smilebase.service.interfaces;
 
+import de.vd40xu.smilebase.dto.PatientDTO;
 import de.vd40xu.smilebase.model.Patient;
 
 import java.util.Optional;
 
 public interface IPatientService {
-    Iterable<Patient> getPatientById();
+    Optional<Patient> getPatientById(long id);
     Optional<Patient> getPatientByInsuranceNumber(String insuranceNumber);
     Optional<Patient> getPatientByEmail(String email);
-    Patient savePatient(Patient patient);
+    Patient savePatient(PatientDTO patient);
     void deletePatient(Long id);
-    Patient updatePatient(Patient patient);
+    Patient updatePatient(PatientDTO patient);
 }
