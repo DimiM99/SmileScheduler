2bf4fff37060b0789a2904661cdf827bf9e54bcd
shawn-ii <106849147+shawn-ii@users.noreply.github.com>
2024-09-21 12:00:21 +0200

    removed props on calendar.tsx

--- Happy Face/src/components/ui/calendar.tsx
+++ Happy Face/src/components/ui/calendar.tsx
@@ -13,6 +13,7 @@ function Calendar({
                       showOutsideDays = true,
                       ...props
                   }: CalendarProps) {
+    
     return (
         <DayPicker
             showOutsideDays={showOutsideDays}
@@ -58,8 +59,10 @@ function Calendar({
                 ...classNames,
             }}
             components={{
-                IconLeft: ({...props}) => <ChevronLeftIcon className="h-4 w-4"/>,
-                IconRight: ({...props}) => <ChevronRightIcon className="h-4 w-4"/>,
+
+                IconLeft: () => <ChevronLeftIcon className="h-4 w-4"/>,
+
+                IconRight: () => <ChevronRightIcon className="h-4 w-4"/>,
             }}
             {...props}
         />
