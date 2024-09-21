650f5ac00bbc9e83b255a46a7a35f391f9f1d3bf
shawn-ii <106849147+shawn-ii@users.noreply.github.com>
2024-09-21 11:59:52 +0200

    small adjustments

--- Happy Face/src/components/ui/Form/index.ts
+++ Happy Face/src/components/ui/Form/index.ts
@@ -1,7 +1,7 @@
-export { default as Form } from "./Form";
-export { default as FormField } from "./FormField";
-export { default as FormItem } from "./FormItem";
-export { default as FormLabel } from "./FormLabel";
-export { default as FormControl } from "./FormControl";
-export { default as FormDescription } from "./FormDescription";
-export { default as FormMessage } from "./FormMessage";
+export { default as Form } from "./form";
+export { default as FormField } from "./formField";
+export { default as FormItem } from "./formItem";
+export { default as FormLabel } from "./formLabel";
+export { default as FormControl } from "./formControl";
+export { default as FormDescription } from "./formDescription";
+export { default as FormMessage } from "./formMessage";
