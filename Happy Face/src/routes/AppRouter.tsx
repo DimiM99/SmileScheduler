fe3f2675ea30525db9c6ed8de65d10e5fba7dad2
Veit Brunnhuber <vxbdeveloper@gmail.com>
2024-09-22 22:53:25 +0200

    Extended Routing with adminDashboard

--- Happy Face/src/routes/AppRouter.tsx
+++ Happy Face/src/routes/AppRouter.tsx
@@ -1,32 +1,43 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
-import Login from '../pages/Login';
-import RecDashboard from "../pages/RecDashboard.tsx";
-import DocDashboard from "../pages/DocDashboard.tsx";
-import PatDashboard from "../pages/PatDashboard.tsx";
+import Login from '@/pages/Login';
+import RecDashboard from "@/pages/RecDashboard.tsx";
+import DocDashboard from "@/pages/DocDashboard.tsx";
+import PatDashboard from "@/pages/PatDashboard.tsx";
 import ProtectedRoute from './ProtectedRoute';
 import TokenProtectedRoute from './TokenProtectedRoute';
+import AdminDashboard from "@/pages/AdminDashboard.tsx";
 
 const AppRouter: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/login" element={<Login/>}/>
+
+                <Route path="/admin-dashboard" element={
+                    <ProtectedRoute>
+                        <AdminDashboard/>
+                    </ProtectedRoute>
+                }/>
+
                 <Route path="/receptionist-dashboard" element={
                     <ProtectedRoute>
                         <RecDashboard/>
                     </ProtectedRoute>
                 }/>
+
                 <Route path="/doctor-dashboard" element={
                     <ProtectedRoute>
                         <DocDashboard/>
                     </ProtectedRoute>
                 }/>
-                <Route path="/appointments" element={
+
+                <Route path="/patient-dashboard" element={
                     <TokenProtectedRoute>
                         <PatDashboard/>
                     </TokenProtectedRoute>
                 }/>
+
                 <Route path="*" element={<Navigate to="/login" replace/>}/>
             </Routes>
         </Router>
