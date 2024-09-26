1cdf386465f4a7c0c5dd5b6391ea683a25dc4abf
shawn-ii <106849147+shawn-ii@users.noreply.github.com>
2024-09-26 21:23:49 +0200

    refactored axios into two separate instances

--- Happy Face/src/services/apiConfig.ts
+++ Happy Face/src/services/apiConfig.ts
@@ -4,19 +4,26 @@ import { AuthService } from '@/services/authService.ts';
 
 export const API_URL: string = 'http://localhost:8080';
 
-export const api: AxiosInstance = axios.create({
-    baseURL: API_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-});
-
-// Attach token to all requests
-api.interceptors.request.use(async (config) => {
-    const authService = new AuthService();
-    const token = await authService.getDecryptedToken();
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+function createApi(useInterceptor: boolean = true) {
+    const api: AxiosInstance = axios.create({
+        baseURL: API_URL,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+    if (useInterceptor) {
+        api.interceptors.request.use(async (config) => {
+            const authService = new AuthService();
+            const token = await authService.getDecryptedToken();
+            if (token) {
+                config.headers.Authorization = `Bearer ${token}`;
+            }
+            return config;
+        });
     }
-    return config;
-});
+    return api;
+}
+
+export const api = createApi();
+
+export const apiWithoutAuth = createApi(false);
