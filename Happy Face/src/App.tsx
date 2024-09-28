4ae4ef5df1f9c4e22c2350d9d7e0d17f52237b76
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-09-28 20:49:22 +0200

    dropping token auth support

--- Happy Face/src/App.tsx
+++ Happy Face/src/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import {AuthProvider} from './contexts/AuthContext';
-import {TokenAuthProvider} from './contexts/TokenAuthContext';
 import AppRouter from './routes/AppRouter';
 
 const App: React.FC = () => {
     return (
         <AuthProvider>
-            <TokenAuthProvider>
-                <AppRouter/>
-            </TokenAuthProvider>
+            <AppRouter/>
         </AuthProvider>
     );
 };
