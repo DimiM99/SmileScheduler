f66838afde2aa96690a19ebf6ed4f875716c7c0d
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-09-28 20:49:59 +0200

    adding a util function for error handling an async calls

--- Happy Face/src/lib/utils.ts
+++ Happy Face/src/lib/utils.ts
@@ -4,3 +4,13 @@ import {twMerge} from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
     return twMerge (clsx (inputs))
 }
+
+export function handleAsyncErrors<A extends unknown[]>(p: (...args: A) => Promise<void>): (...args: A) => void {
+  return (...args: A) => {
+    try {
+      p(...args).catch((err: unknown) => { console.log("Error thrown asynchronously", err); })
+    } catch (err) {
+      console.log("Error thrown synchronously", err)
+    }
+  }
+}
