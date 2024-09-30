23486992e759473bc6a87fc7b0e5ca171ebdc947
Dmitri Moscoglo <dmmoskoglo@gmail.com>
2024-09-30 11:13:49 +0200

    added fetching doctors into appointmentService and fixed wrong implementation of the update function adding the correct request model for its parameter

--- Happy Face/src/models/services/IAppointmentService.ts
+++ Happy Face/src/models/services/IAppointmentService.ts
@@ -1,9 +1,11 @@
-import {AppointmentRequest} from "@/models";
+import {AppointmentRequest, Doctor} from "@/models";
 import {AppointmentResponse} from "@/models/services/responses/AppointmentResponse.ts";
+import {AppointmentUpdateRequest} from "@/models/services/requests/AppointmentUpdateRequest.ts";
 
 export interface IAppointmentService {
     createAppointment: (appointmentRequest: AppointmentRequest) => Promise<AppointmentResponse>;
     getAppointment: (appointmentId: number) => Promise<AppointmentResponse>;
-    updateAppointment: (appointmentRequest: AppointmentRequest) => Promise<AppointmentResponse>;
+    updateAppointment: (appointmentRequest: AppointmentUpdateRequest) => Promise<AppointmentResponse>;
     deleteAppointment: (appointmentId: number) => Promise<void>;
+    fetchDoctors: () => Promise<Doctor[]>;
 }
