e8b8cc751600a7a6083a14499b501986c09cd29e
shawn-ii <106849147+shawn-ii@users.noreply.github.com>
2024-09-30 17:07:07 +0200

    updated IAppointmentService.ts

--- Happy Face/src/models/services/IAppointmentService.ts
+++ Happy Face/src/models/services/IAppointmentService.ts
@@ -1,9 +1,12 @@
 import {AppointmentRequest} from "@/models";
 import {AppointmentResponse} from "@/models/services/responses/AppointmentResponse.ts";
+import {AppointmentType} from "@/models/enums/AppointmentType.ts";
 
 export interface IAppointmentService {
+    getFreeSlots: (doctorId: number, date: string, appointmentType: AppointmentType, weekView?: boolean) => Promise<string[]>;
     createAppointment: (appointmentRequest: AppointmentRequest) => Promise<AppointmentResponse>;
     getAppointment: (appointmentId: number) => Promise<AppointmentResponse>;
+    getAppointmentsForDoctor: (doctorId: number, date: string, weekView?: boolean) => Promise<AppointmentResponse[]>;
     updateAppointment: (appointmentRequest: AppointmentRequest) => Promise<AppointmentResponse>;
     deleteAppointment: (appointmentId: number) => Promise<void>;
 }
